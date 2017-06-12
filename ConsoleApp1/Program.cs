a53fe295aef73bc509a9b8fa976d64057d8fbc0d
Kolontar Igor <tuxuuyzhas@outlook.com>
2017-06-12 13:31:31 +0400

    ~realised init

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.IO;
 
@@ -10,18 +11,54 @@ namespace fb2renamer
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        static void Init()
         {
-            Console.WriteLine("FB2Renamer v. 0.1 готов к работе");
+            int fb2count = 0;
             string currDir = Directory.GetCurrentDirectory();
             string[] fileEntries = Directory.GetFiles(currDir);
-            Console.WriteLine(currDir);
-            Console.ReadKey();
-            foreach (string filename in fileEntries)
+            Console.WriteLine("FB2Renamer v. 0.1 готов к работе");
+            Console.WriteLine("Рабочая папка: {0}", currDir);
+            Console.WriteLine("Файлов в папке: {0}", fileEntries.Length);
+            foreach (string fName in fileEntries)
             {
-                Console.WriteLine(filename);
+                if (Path.GetExtension(fName) == ".fb2")
+                {
+                    fb2count += 1;
+                    Console.WriteLine(Path.GetFileName(fName));
+
+                    //TODO: сделать очистку имени файла от мусорных символов, не равных "." "А-Я" "A-Z"
+                    //TODO: сделать транслитерацию названий файла по желанию пользователя
+                }
+            }
+            Console.WriteLine("Книг fb2 найдено: {0}", fb2count);
+
+            Console.ReadLine();
+        }
+
+
+        public string CleanedFileName(string filename)
+            
+        {
+            Regex regex = new Regex("([a-zA-Zа-яА-Я])");
+            char[] array = filename.ToCharArray();
+            for (int i=0; i<=filename.Length; i++)
+            {
+                array[i] = ;
             }
 
+            filename = filename.Trim();
+            return filename;
+        }
+
+        public void LoadBookData()
+        {
+            
+        }
+        
+        static void Main(string[] args)
+        {
+            Init();   
         }
     }
 }
